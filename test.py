678c92e0561b4bffff71fef0c6590c379f00d750
mdogdope <m45t3rhest@gmail.com>
2018-02-15 13:04:37 -0800

    The algo to find the total worth of the amount not traded works

--- test.py
+++ test.py
@@ -7,21 +7,4 @@ import json
 x = TotalCost()
 x.setcoin("DSH")
 print (x.getpaid())
-#print(x.getbal())
-'''
-config = configparser.ConfigParser()
-config.sections()
-config.read('keys.ini')
-public_key = config['KEYS']['Public']
-secret = config['KEYS']['Private']
-
-
-session = requests.session() 
-session.auth = (public_key, secret)
-
-_trades = session.get('https://api.hitbtc.com/api/2/history/trades?symbol=DSHBTC').json()
-
-for _trades in _trades:
-	if (_trades['symbol'] == "DSHBTC"):
-		print(_trades['quantity'])
-'''
+print(x.getbal())
